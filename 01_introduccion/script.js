2f7cae83039c520730029fc667b864693fdf923f
caarlosdamian <dapecarlos@gmail.com>
2024-02-19 22:03:55 -0600

    feat(js): 19-feb-2024

---
+++ 01_introduccion/script.js
@@ -0,0 +1,146 @@
+// let esVerdadero = true;
+
+// var nombre = "Bob";
+// console.log('Nombre ' +  nombre);
+// nombre = "John";
+// console.log('Nombre ' +  nombre);
+
+// console.log('esVerdadero ' +  esVerdadero);
+// esVerdadero = false
+// console.log('esVerdadero ' +  esVerdadero);
+// // [PALABRA RESERVADA]  {IDENTIFICADOR(NOMBRE QUE LE PONEMOS)} = VALOR
+// // VARIABLE SE PUEDE CAMBIAR
+
+// // CONSTANTE => NO SE PUEDE CAMBIAR;
+// const numeroTreintaYCinco = 35;
+
+// // LET => CONST
+
+// console.log('numeroTreintaYCinco ' +  numeroTreintaYCinco);
+
+// // strings
+// const js = 'Javascript';
+// const comillaDoble = 'John ${js}';
+const comillaSimple = 'Name ${js}';
+// const comillaInversa = `Agregar ${js}`; // Template String
+// const comillaInversaSuma = `Agregar ${5 + 5}`; // Template String
+
+// console.log(comillaInversa);
+// console.log(comillaSimple);
+// console.log(comillaDoble);
+// console.log(comillaInversaSuma);
+
+// // numbers #
+// // int public numeroEntero =
+// // float public numerosDecimales =
+// const numeroEntero = 10;
+// let numeroAlazar = 23;
+// numeroAlazar = 35.5;
+// const numerosDecimales = 10.6893840938320283904823;
+// console.log(numerosDecimales);
+// // console.log(typeof comillaSimple)
+
+// boolean
+
+// const esMayorDeEdad = false; //
+// const nombre = 'Gustavo Ceballos'
+// if(esMayorDeEdad){
+//     // ejecutar esto si lo de arriba es verdadero
+//     // en otras palabras si es TRUE
+//     console.log(`${nombre}, eres mayor de edad`)
+// } else {
+//     console.log(`${nombre}, no eres mayor de edad`)
+//       // ejecutar esto si lo de arriba es falso
+//     // en otras palabras si es FALSE
+// }
+
+// null
+
+// let age = null;
+
+// console.log(age);
+
+// const handleClick = () => {
+//     console.log('Prueba')
+//     age = 22;
+//     console.log(age);
+// }
+// console.log(age);
+
+// const button = document.getElementsByTagName('button');
+// button[0].addEventListener('click',handleClick)
+
+// let x; // mala practica DONT DO IT
+
+// console.log(x)
+
+// objectos
+// SINTAXIS
+// [llave] : VALOR,
+// // [llave] : VALOR,
+// const person = {
+//   name: 'Jonh',
+//   age: 20,
+// };
+
+// // console.log(person.age)
+// // console.log(person.name)
+
+// console.log(person['age'])
+// console.log(person['name'])
+
+// Operadores
+
+// const a = 13;
+// const b = 12;
+
+// resultado
+let result = 0;
+
+// suma
+// result = a + b;
+// result += a + b
+// =>
+// resta
+// result = a - b;
+// result = a - b;
+// multiplicacion
+// result = a * b;
+// exponencial
+// result = 2 ** 2;
+// modulo o resto
+// result = a % b;
+// bisiestos
+// divicion
+// result = a / b;
+// console.log(result);
+// operadores logicos 
+
+
+const a = 13;
+const b = 12;
+
+
+
+// console.log('10' == 10); // true string numerico 10 
+// coercion de typos 
+// '10'
+// '10'  =>true 
+// console.log('10' === 10);
+// console.log(0 == "");
+// console.log("" == "0");
+
+
+// falsy values 
+// logical operators 
+// console.log(false && true) //  AND LOS DOS DEBEN DE SER VERDADEROS  => TRUE
+// console.log(false || true) //  OR ALMENOS UNO DEBE DE SER VERDADERO  => TRUE
+// console.log(false || true || true) //  OR ALMENOS UNO DEBE DE SER VERDADERO  => TRUE
+// console.log(false || true && true) //  OR ALMENOS UNO DEBE DE SER VERDADERO  => TRUE
+// negador
+//negacion 
+// negacion doble 
+console.log(!!true)
+console.log(!false)
+console.log(!!undefined)
+
